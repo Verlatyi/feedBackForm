7ac0821a6154ea3338bf58fb0c4ced96f94d76b9
lucky <luckydev2020@gmail.com>
2020-06-23 01:53:10 +0300

    own captcha ready for client

--- script.js
+++ script.js
@@ -3,24 +3,39 @@
 let fieldName = document.querySelectorAll("input")[0],
   fieldPhone = document.querySelectorAll("input")[1],
   fieldEmail = document.querySelectorAll("input")[2],
-  btnSubmit = document.querySelector("button");
+  fieldRadioBtn = document.querySelectorAll("input")[3],
+  btnSubmit = document.querySelector("button"),
+  numberPhone;
 
 function btnNoActive() {
   btnSubmit.style.background = "grey";
   btnSubmit.disabled = true;
 }
 
+btnNoActive();
+
 function btnActive() {
   btnSubmit.style.background = "lightgreen";
   btnSubmit.disabled = false;
 }
 
-btnSubmit.addEventListener("click", (e) => {
-  if (fieldName.value.length < 5) {
-    alert(fieldName.value.length);
-  } else {
-    btnActive();
+fieldRadioBtn.addEventListener("click", (e) => {
+  numberPhone = /^\d+$/.test(fieldPhone.value);
+
+  if (fieldRadioBtn.checked == true) {
+    if (
+      fieldName.value.length < 2 ||
+      fieldPhone.value.length < 9 ||
+      fieldPhone.value.length > 12 ||
+      fieldName.value.length > 20 ||
+      fieldEmail.value.length < 5 ||
+      fieldEmail.value.length > 30 ||
+      numberPhone == false
+    ) {
+      alert("Поля заполненны некоректно! Внесите пожалуйста исправления.");
+      fieldRadioBtn.checked = false;
+    } else {
+      btnActive();
+    }
   }
 });
-
-console.log(fieldName.placeholder);
